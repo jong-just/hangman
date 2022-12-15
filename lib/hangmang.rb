10b329003bc6b277387b288d0b4438ce6e1972ec
justin h <joosteen96@gmail.com>
2022-12-14 19:24:45 -0800

    Finish core game mechanic
    Need to add input validation and look for any
    potential bugs.

--- lib/hangmang.rb
+++ lib/hangmang.rb
@@ -6,6 +6,8 @@ module Toolbox
   def open_csv
     CSV.open('word_dictionary.csv', headers: false)
   end
+
+  MAXROUNDS = 6
 end
 
 # class to hold player's data
@@ -22,6 +24,10 @@ class Hangman
   include Toolbox
 
   @@answer_word_scrambled_array = []
+  @@incorrect_count = 0
+  @@win = false
+  @@lose = false
+  @@end = false
 
   private
 
@@ -64,21 +70,26 @@ class Hangman
   end
 
   def check_player_guess
-    
+    good_letter = false
+    count = 0
+    @@guess_word.split('').each_with_index do |letter, idx|
+      if letter == @@player_guess.word
+        @@answer_word_scrambled_array[idx] = letter
+        good_letter = true
+        count+=1
+      end
+    end
 
-    puts "Guess word array: #{@@answer_word_scrambled_array}"
-  end
-
-  public
-
-  def test_output
-    puts @@guess_word
-  end
-
-  def test_output_scrambled
-    generate_word
-    scramble_guess_word
-    puts @@answer_word_scrambled_array.join
+    if good_letter == false
+      puts "#{@@player_guess.word} was not in the word."
+      @@incorrect_count+=1
+    elsif good_letter
+      puts "#{@@player_guess.word} was in #{count} time[s]."
+    else
+      puts "Uh-oh. ERRRORR!!"
+    end
+    puts "Guess word array: #{@@answer_word_scrambled_array.join}"
+    p @@guess_word
   end
 
   def output_scrambled
@@ -96,11 +107,58 @@ class Hangman
   end
 
   def play_round
+    get_player_guess
+    check_player_guess
+  end
+
+  def game_setup
     generate_word
     scramble_guess_word
     output_scrambled
     get_initial_player_guess
     check_player_guess
+    output_chances_left
+  end
+
+  def output_chances_left
+    puts "You have #{MAXROUNDS-@@incorrect_count}/#{MAXROUNDS} chances left."
+  end
+
+  def win_check
+    if @@guess_word == @@answer_word_scrambled_array.join
+      @@end = true
+      @@win = true
+    end
+  end
+
+  def lose_check
+    if @@incorrect_count == MAXROUNDS
+      @@end = true
+      @@lose = true
+    end
+  end
+
+  def end_condition_check
+    lose_check
+    win_check
+  end
+
+  public
+
+  def play_game
+    game_setup
+    until @@end do
+      play_round
+      end_condition_check
+      output_chances_left
+    end
+    if @@lose
+      puts "You lose! The word was #{@@guess_word}."
+    elsif @@win
+      puts "You win! The word was #{@@guess_word}"
+    else
+      puts "Uh-oh, another error at play_game!!"
+    end
   end
 end
 
@@ -108,4 +166,4 @@ puts "Game starting!"
 
 new_game = Hangman.new
 
-new_game.play_round
+new_game.play_game
